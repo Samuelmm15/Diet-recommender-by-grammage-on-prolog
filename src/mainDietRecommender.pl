747b93c90891b78c3c989ddacd7eae2ef966326c
Samuel Martín <alu0101359526@ull.edu.es>
2023-01-07 16:31:47 +0000

    Updated - to obtain the contentn of a specific data base sentence

--- src/mainDietRecommender.pl
+++ src/mainDietRecommender.pl
@@ -5,91 +5,59 @@
 /* Diet Recommender by grammage System */
 /* -------------------------- */
 
-:- initialization main.
-
-main:-
-    %  En este punto se deben de poner las funciones que va a seguir el programa, es decir, las reglas que va a seguir el programa para llegar a la solucion.
-    userInterface,
-    collectingTheData.
-    
-
-userInterface:-
-    %  En este punto se debe de poner la interfaz que va a usar el usuario para comunicarse con el programa.
-    nl,
-    write('------------------------------------'),nl,
-    write('Diet Recommender by grammage System'),nl,
-    write('------------------------------------'),nl,
-    nl,
-    write('Hello, my name is Samuel Martín Morales, this is my diet recommender system, and I am going to help you with your diet.'),nl,
-    write('Please answer the following questions.'),nl,
-    nl.
-    
-
-collectingTheData:-
-    %  En este punto se debe de poner la recopilacion de datos que va a usar el programa para llegar a la solucion.
-    write('How old are you? '),nl,
-    read(AGE),nl,
-    write('What is your height in centimeters? '),nl,
-    read(HEIGHT),nl,
-    write('What is your weight in kilograms? '),nl,
-    read(WEIGHT),nl,
-    write('Finally the recommended diet for you is: '),nl,
-    %  En este punto se debe de poner la solucion que va a dar el programa.
-    recommendation(DIET).
-
 % En este punto se deben encontrar las reglas dependiendo de las preguntas que se realizan al usuaio.
 
 % Relación de los distintos alimentos los cuales se pueden consumir.
 % Verduras.
-lettuce(lettuce).
-tomatoe(tomatoe).
-cucumber(cucumber).
-carrot(carrot).
-onion(onion).
-pepper(pepper).
-potatoe(potatoe).
+lettuce("lettuce").
+tomatoe("tomatoe").
+cucumber("cucumber").
+carrot("carrot").
+onion("onion").
+pepper("pepper").
+potatoe("potatoe").
 % Frutas.
-apple(apple).
-orange(orange).
-banana(banana).
-strawberry(strawberry).
-raspberry(raspberry).
-blueberry(blueberry).
-watermelon(watermelon).
+apple("apple").
+orange("orange").
+banana("banana").
+strawberry("strawberry").
+raspberry("raspberry").
+blueberry("blueberry").
+watermelon("watermelon").
 % Cereales.
-rice(rice).
-pasta(pasta).
-bread(bread).
-corn(corn).
-oatmeal(oatmeal).
+rice("rice").
+pasta("pasta").
+bread("bread").
+corn("corn").
+oatmeal("oatmeal").
 % Carnes.
-chicken(chicken).
-beef(beef).
-pork(pork).
-fish(fish).
+chicken("chicken").
+beef("beef").
+pork("pork").
+fish("fish").
 % Lácteos.
-milk(milk).
-cheese(cheese).
-yogurt(yogurt).
+milk("milk").
+cheese("cheese").
+yogurt("yogurt").
 % Legumbres.
-beans(beans).
-lentils(lentils).
-peas(peas).
+beans("beans").
+lentils("lentils").
+peas("peas").
 % Aceites.
-oliveOil(oliveOil).
-sunflowerOil(sunflowerOil).
-cornOil(cornOil).
+oliveOil("oliveOil").
+sunflowerOil("sunflowerOil").
+cornOil("cornOil").
 % Bebidas.
-water(water).
-juice(juice).
-coffee(coffee).
-tea(tea).
+water("water").
+juice("juice").
+coffee("coffee").
+tea("tea").
 % Otros.
-egg(egg).
-honey(honey).
-sugar(sugar).
-salt(salt).
-vinegar(vinegar).
+egg("egg").
+honey("honey").
+sugar("sugar").
+salt("salt").
+vinegar("vinegar").
 
 % Relación de las distintas comidas del día.
 % Desayuno.
@@ -121,11 +89,50 @@ dinner3().
 dinner4().
 dinner5().
 
+
+:- initialization main.
+
+main:-
+    %  En este punto se deben de poner las funciones que va a seguir el programa, es decir, las reglas que va a seguir el programa para llegar a la solucion.
+    userInterface,
+    collectingTheData.
+    
+
+userInterface:-
+    %  En este punto se debe de poner la interfaz que va a usar el usuario para comunicarse con el programa.
+    nl,
+    write('------------------------------------'),nl,
+    write('Diet Recommender by grammage System'),nl,
+    write('------------------------------------'),nl,
+    nl,
+    write('Hello, my name is Samuel Martín Morales, this is my diet recommender system, and I am going to help you with your diet.'),nl,
+    write('Please answer the following questions.'),nl,
+    nl.
+    
+
+collectingTheData:-
+    %  En este punto se debe de poner la recopilacion de datos que va a usar el programa para llegar a la solucion.
+    write('How old are you? '),nl,
+    read(AGE),nl,
+    write('What is your height in centimeters? '),nl,
+    read(HEIGHT),nl,
+    write('What is your weight in kilograms? '),nl,
+    read(WEIGHT),nl,nl,
+    write('Finally the recommended diet for you is: '),nl,
+    %  En este punto se debe de poner la solucion que va a dar el programa.
+    breakfast(Get), % DE ESTA MANERA ES COMO SE OBTIENE EL CONTENIDO DE LA LISTA.
+    write('Breakfast: '),nl,
+    write(Get),nl,
+    write('Lunch: '),nl,
+    write('Snack: '),nl,
+    write('Dinner: '),nl,
+    write('Thank you for using my diet recommender system, I hope you have a good day!'),nl,nl.
+
 % Base de datos de las distintas dietas con los distintos elementos.
 
 % En este punto se deben de encontrar las recomendaciones que se deben de realizar, es decir,
 % la base de datos, de las distintas dietas que se van a dar como recomendaciones.
-recommendation(breakfast).
+% recommendation(breakfast).
 
 
 % ESTO ES UN EJEMPLO DE IMPLEMENTACIÓN DE INTERFAZ GRÁFICA DE USUARIO EN PROLOG (PROBAR EN WINDOWS PARA
